55490914563aa88aa9c94e904623488c2ae6a46a
Lhie Xhian <lhiexhian@gmail.com>
2025-07-07 13:33:03 +0800

    Create a class

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Ticu
+{
+    class Program
+    {
+        public class Ticu
+        {
+            private string _name;
+            private int _age;
+            private double _hunger;
+            private string _species;
+
+            public Ticu(string name, int age, double hunger, string species)
+            {
+                _name = name;
+                _age = age;
+                _hunger = hunger;
+                _species = species;
+            }
+
+            public void Feed(double foodAmount)
+            {
+                // feed
+            }
+            public void Play()
+            {
+                // play
+            }
+            public void Sleep()
+            {
+                // sleep
+            }
+            public string PrintInfo()
+            {
+                return $"Name: {_name}, Age: {_age}, Hunger: {_hunger}, Species: {_species}";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Main arguments
+            var micu = new Ticu("Micu", 2, 0.5, "Cat");
+            Console.WriteLine(micu.PrintInfo());
+
+        }
+    }
+}
